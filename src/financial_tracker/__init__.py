5dbd832766ab26ff2d2310f3b7b9c599e88b468c
Andre <aborczuk@gmail.com>
2026-07-20 15:28:09 -0400

    Scaffold financial tracker package

---
+++ src/financial_tracker/__init__.py
@@ -0,0 +1,8 @@
+"""Financial Acceleration Tracker package boundary and public metadata."""
+
+from __future__ import annotations
+
+PACKAGE_NAME = "financial_tracker"
+__version__ = "0.1.0"
+
+__all__ = ["PACKAGE_NAME", "__version__"]
